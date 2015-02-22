3998d4971927b8caaf9ec8ec289d488fccf761f0
Tyler '-z-' Mulligan <z@xnz.me>
2015-02-21 21:31:51 -0500

    first commit with vecxis as an example

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cacs
 cvar and cmd search for darkplaces based games
+
+Current example shown with Vecxis data
---
+++ js/app.js
@@ -0,0 +1,30 @@
+require.config({
+    baseUrl: 'js/lib',
+    
+    paths: {
+        jquery: 'jquery',
+        datatables: 'jquery.dataTables.min',
+        dtbootstrap: 'dataTables.bootstrap',
+        transition: 'transition',
+        collapse: 'collapse',
+    },
+    shim: {
+        datatables: {
+            deps: ['jquery']
+        },
+        cookie: {
+            deps: ['jquery']
+        },
+        dtbootstrap: {
+            deps: ['datatables']
+        },
+        transition: {
+            deps: ['dtbootstrap']
+        },
+        collapse: {
+            deps: ['dtbootstrap']
+        }
+    }
+});
+
+requirejs(['../app/main']);
